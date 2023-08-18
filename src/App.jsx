547f08421985e6ad4378e0f83054a166ee2d78b3
saymanrabbi <mdrabbi.asm@gmail.com>
2023-08-18 10:42:11 +0600

    add form data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import AddDataForm from './Components/AddDataForm'
 
 function App() {
 
   return (
     <div>
-      <h2 className=' text-red-500 font-bold'>
-        Hello World
-      </h2>
+        <AddDataForm />
     </div>
   )
 }
---
+++ src/Components/AddDataForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AddDataForm = () => {
+    return (
+        <div className=' w-[100%] h-[400px] bg-red-400'>
+             <div className=' w-[50%] mx-auto h-full bg-black'>
+
+             </div>
+        </div>
+    );
+};
+
+export default AddDataForm;
