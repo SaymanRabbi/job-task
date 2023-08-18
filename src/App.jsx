acd22dbb4a558db1e8fd1b48268e1a3cbb28f6f6
saymanrabbi <mdrabbi.asm@gmail.com>
2023-08-18 10:36:27 +0600

    add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <h2 className=' text-red-500 font-bold'>
+        Hello World
+      </h2>
+    </div>
   )
 }
 
