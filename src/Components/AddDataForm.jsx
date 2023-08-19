7d2393c125f53fb273b52f8d5afa9f36344ac00e
saymanrabbi <mdrabbi.asm@gmail.com>
2023-08-20 00:29:42 +0600

    checkbox working

--- src/Components/AddDataForm.jsx
+++ src/Components/AddDataForm.jsx
@@ -1,11 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Data from './Data';
 
 const AddDataForm = () => {
+    const [name,setName]=useState('Select Category');
+    const [value,setValue]=useState('Select Sub Category');
+    const [agree,setAgree]=useState(
+        name === 'Select Category' || value === 'Select Sub Category' ? false : true
+        );
+    const [checked,setChecked]=useState(false);
+    const HandelChange = (e) => {
+        setName(e.target.value);
+    }
+    const HandelValueChange = (e) => {
+        setValue(e.target.value);
+    }
+    const HandelAgree = () => {
+        setChecked(!checked);
+        if(name === 'Select Category' || value === 'Select Sub Category'){
+          return  setAgree(false);
+    }
+    setAgree(!agree);
+}
+    const HandelSubmit = (e) => {
+        e.preventDefault();
+    }
+    useEffect(()=>{
+        Data.map((item)=>{
+            if(item.name === name){
+                setValue(item.value[0]);
+            }
+        })
+       
+            if((name === 'Select Category' || value === 'Select Sub Category') && (checked)){
+                return setAgree(false)
+            }
+            if((name !== 'Select Category' || value !== 'Select Sub Category') && (checked)){
+                return setAgree(true)
+            }
+        
+    },[name,value])
     return (
         <div className='w-[100%] bg-gradient-to-tr from-[#C4DAFE] to-[#EEF5FF]'>
             <div className='px-8 py-8'>
-            <div className='w-[100%] h-[calc(89vh-60px)] bg-white rounded-lg shadow-lg'>
-                    <form className=' w-[50%] mx-auto py-14'>
+            <div className='w-[100%]  bg-white rounded-lg shadow-lg'>
+                    <form className=' md:w-[50%] w-[100%] mx-auto py-14 px-4 md:px-0'
+                    onSubmit={HandelSubmit}
+                    >
                         {/* --------form title-------- */}
                         <h2 className=' font-[600]'>Please enter your name and pick the Sectors you are currently involved in.</h2>
                         {/* --------form title-------- */}
@@ -19,22 +59,51 @@ const AddDataForm = () => {
                          <div className='mt-8'>
                             <label className=' block font-[450]'>Choose the category and sub-categor you are currently involved in.</label>
                             <div className=' flex mt-8 gap-8'>
-                                 <select  className=' w-[40%] py-1 outline-none border-2 border-blue-600 rounded-md'>
-                                    <option value="">Hello</option>
+                                 <select  className=' md:w-[40%] w-[50%] py-1 outline-none border-2 border-blue-600 rounded-md'
+                                 onChange={HandelChange}
+                                 defaultValue={name}
+                                 >
+                                    {
+                                        Data.map((item,index)=>{
+                                            return(
+                                                <option  key={index} value={item.name}
+                                                 
+                                                >{item.name}</option>
+                                            )
+                                        })
+                                    }
                                  </select>
-                                 <select  className='w-[40%] py-1 outline-none border-2 border-blue-600 rounded-md'>
-                                    <option value="">Hello</option>
+                                 <select  className='md:w-[40%] w-[50%] py-1 outline-none border-2 border-blue-600 rounded-md'
+                                  onChange={HandelValueChange}
+                                    defaultValue={value}
+                                 >
+                                    {
+                                        Data.map((item)=>{
+                                            return (
+                                                item.name === name && item.value.map((item,index)=>{
+                                                    return(
+                                                        <option key={index} value={item}>{item}</option>
+                                                    )
+                                                })
+                                            )
+                                        })
+                                    }
                                  </select>
                             </div>
                          </div>
                         {/* --------Input for select ocopotion------ */}
-                         <div class="flex items-center mb-4 mt-6">
-                        <input id="checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500  focus:ring-2 dark:bg-gray-700 "/>
-                        <label for="checkbox" class="ml-2 text-sm font-[600] text-gray-900 ">Agree to terms</label>
+                         <div className="flex items-center mb-4 mt-6">
+                        <input id="checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500  focus:ring-2 dark:bg-gray-700 "
+                         onClick={(e)=>HandelAgree(e)}
+                        />
+                        <label htmlFor="checkbox" className="ml-2 text-sm font-[600] text-gray-900 ">Agree to terms</label>
                         </div>
                         {/* ------save button------ */}
                         <div className=' relative z-[1] overflow-hidden w-[100px] text-center'>
-                            <button className='py-2 rounded-md button border border-gray-400 w-full hover:text-white'>Submit</button>
+                            <button className={`py-2 rounded-md  border border-gray-400 w-full  ${agree?"button hover:text-white":" bg-gray-200 text-gray-300"}`}
+                            disabled={!agree}
+                            type='submit'
+                            >Submit</button>
                         </div>
                         {/* ------save button------ */}
                     </form>
---
+++ src/Components/Data.js
@@ -0,0 +1,33 @@
+const Data = [
+    {
+        id: 0,
+        name: "Select Category",
+        value: [
+            "Select Sub Category"
+        ]
+    },
+     {
+            id: 1,
+            name: "Manufacturing",
+            value:[
+                "Construction & Engineering",
+                "Electronics & Electricals",
+                "Food & Beverages",
+                "Healthcare & Pharmaceuticals",
+                "Industrial Machinery",
+                "Metals & Mining",
+            ]
+     },{
+            id: 2,
+            name: "Furniture",
+            value:[
+                "Bathroom Furniture",
+                "Bedroom Furniture",
+                "Children Furniture",
+                "Commercial Furniture",
+                "Dining Room Furniture",
+            ]
+     }
+]
+
+export default Data;
